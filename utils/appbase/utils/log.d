dd638f74ffa965bb627dbcc451d6a2af80150118
shove70 <shove@163.com>
2019-09-16 12:23:32 +0800

    Upgrade dependencie crypto to v0.2.10

--- utils/appbase/utils/log.d
+++ utils/appbase/utils/log.d
@@ -1,55 +1,36 @@
 module appbase.utils.log;
 
-import std.file;
-import std.path;
-import std.conv;
-import std.string;
-import std.experimental.logger.core;
-import std.experimental.logger.filelogger;
+import std.path : buildPath;
+import std.conv : to;
+import std.experimental.logger.core : LogLevel;
+import std.experimental.logger.filelogger : FileLogger, CreateFolder;
+import std.datetime;
 
 import appbase.utils.utility;
 
-struct logger
-{
-    static string logFile;
+alias logger = Logger;
 
-    static void write(string file = __FILE__, size_t line = __LINE__, Args...)(Args args)
+/// file logger.
+struct Logger
+{
+    /// write log to file.
+    static void write(string file = __FILE__, size_t line = __LINE__,
+        string funcName = __FUNCTION__,
+        string prettyFuncName = __PRETTY_FUNCTION__,
+        string moduleName = __MODULE__,
+        Args...)(Args args)
     {
         if (args.length == 0)
         {
             return;
         }
 
-        string path = buildPath(getExePath(), "log", now.year.to!string);
+        DateTime dt = now;
+        string filename = buildPath(getExePath(), "log", dt.year.to!string, dt.date.toISOString() ~ ".log");
+        auto f = new FileLogger(filename, LogLevel.all, CreateFolder.yes);
 
-        if (!std.file.exists(path))
-        {
-            std.file.mkdirRecurse(path);
-        }
-
-        string filename = buildPath(path, now.date.toISOString() ~ ".log");
-
-        if ((logFile == string.init) || (logFile != filename))
-        {
-            synchronized
-            {
-                if ((logFile == string.init) || (logFile != filename))
-                {
-                    logFile   = filename;
-                    sharedLog = new FileLogger(logFile);
-                }
-            }
-        }
-
-        string msg;
-        foreach (i, arg; args)
-        {
-            msg ~= arg.to!string();
-
-            if (i > 0)
-                msg ~= ", ";
-        }
-
-        log(getExeName ~ "/" ~ file ~ ":" ~ line.to!string ~ ": " ~ msg);
+        f.log!(line, file, funcName, prettyFuncName, moduleName)(getExeName, ", ", args);
+        f.file.flush();
+        f.file.close();
     }
 }
