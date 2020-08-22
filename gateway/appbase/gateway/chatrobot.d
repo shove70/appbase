2b3586ad9f11c56b6059bb6b23bc6ccb4ea31568
shove70 <shove@163.com>
2020-08-22 11:17:49 +0800

    Add catrobot (dingding, weCom).

---
+++ gateway/appbase/gateway/chatrobot.d
@@ -0,0 +1,29 @@
+module appbase.gateway.chatrobot;
+
+import std.net.curl : HTTP;
+import std.typecons : No;
+
+/// Send notify to chat (DingDing, weCom)
+private string sendToChat(string url)(const string key, const string title, const string message)
+{
+    auto http = HTTP(url ~ key);
+    http.addRequestHeader("Content-Type", "application/json");
+    string response;
+    http.onReceive = (ubyte[] data) { response = cast(string) data; return data.length; };
+    http.postData = `{"msgtype":"text","text":{"content":"【` ~ title ~ `】\n` ~ message ~ `"}}`;
+    http.perform(No.throwOnError);
+
+    return response;
+}
+
+/// ditto
+string sendToDingding(const string key, const string title, const string message)
+{
+    return sendToChat!"https://oapi.dingtalk.com/robot/send?access_token="(key, title, message);
+}
+
+/// ditto
+string sendToWeCom(const string key, const string title, const string message)
+{
+    return sendToChat!"https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key="(key, title, message);
+}
--- gateway/appbase/gateway/package.d
+++ gateway/appbase/gateway/package.d
@@ -2,3 +2,5 @@ module appbase.gateway;
 
 public import appbase.gateway.sms;
 public import appbase.gateway.smtp;
+public import appbase.gateway.chatrobot;
+
