fcba80e243a2760bedf771f3a1f8ba89c7f3ede7
shove70 <shove@163.com>
2020-08-24 17:09:12 +0800

    Adjust sendToChat: set markdown as default text type.

--- gateway/appbase/gateway/chatrobot.d
+++ gateway/appbase/gateway/chatrobot.d
@@ -4,26 +4,26 @@ import std.net.curl : HTTP;
 import std.typecons : No;
 
 /// Send notify to chat (DingDing, weCom)
-private string sendToChat(string url)(const string key, const string title, const string message)
+private string sendToChat(string url)(const string key, const string message)
 {
     auto http = HTTP(url ~ key);
     http.addRequestHeader("Content-Type", "application/json");
     string response;
     http.onReceive = (ubyte[] data) { response = cast(string) data; return data.length; };
-    http.postData = `{"msgtype":"text","text":{"content":"【` ~ title ~ `】\n` ~ message ~ `"}}`;
+    http.postData = `{"msgtype":"markdown","markdown":{"content":"` ~ message ~ `"}}`;
     http.perform(No.throwOnError);
 
     return response;
 }
 
 /// ditto
-string sendToDingding(const string key, const string title, const string message)
+string sendToDingding(const string key, const string message)
 {
-    return sendToChat!"https://oapi.dingtalk.com/robot/send?access_token="(key, title, message);
+    return sendToChat!"https://oapi.dingtalk.com/robot/send?access_token="(key, message);
 }
 
 /// ditto
-string sendToWeCom(const string key, const string title, const string message)
+string sendToWeCom(const string key, const string message)
 {
-    return sendToChat!"https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key="(key, title, message);
+    return sendToChat!"https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key="(key, message);
 }
