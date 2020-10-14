c3c14ccf846d2b1c833cd35205f10805dce15462
shove70 <shove@163.com>
2020-10-14 18:07:07 +0800

    Fixes sendMessageToDingding.

--- gateway/appbase/gateway/chatrobot.d
+++ gateway/appbase/gateway/chatrobot.d
@@ -3,9 +3,11 @@ module appbase.gateway.chatrobot;
 import std.net.curl : HTTP;
 import std.typecons : No;
 
-/// Send notify to chat (DingDing, weCom)
-private string sendToChat(string url)(const string key, const string message)
+/// Send notify to WeCom
+string sendToWeCom(const string key, const string message)
 {
+    const string url = "https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key=";
+
     auto http = HTTP(url ~ key);
     http.addRequestHeader("Content-Type", "application/json");
     string response;
@@ -16,14 +18,17 @@ private string sendToChat(string url)(const string key, const string message)
     return response;
 }
 
-/// ditto
+/// Send notify to Dingding
 string sendToDingding(const string key, const string message)
 {
-    return sendToChat!"https://oapi.dingtalk.com/robot/send?access_token="(key, message);
-}
+    const string url = "https://oapi.dingtalk.com/robot/send?access_token=";
 
-/// ditto
-string sendToWeCom(const string key, const string message)
-{
-    return sendToChat!"https://qyapi.weixin.qq.com/cgi-bin/webhook/send?key="(key, message);
+    auto http = HTTP(url ~ key);
+    http.addRequestHeader("Content-Type", "application/json");
+    string response;
+    http.onReceive = (ubyte[] data) { response = cast(string) data; return data.length; };
+    http.postData = `{"msgtype":"markdown","markdown":{"title":"Notify","text":"` ~ message ~ `"}}`;
+    http.perform(No.throwOnError);
+
+    return response;
 }
