d6c05172befb250e109db4b26a29dfb5d57ea761
shove70 <shove@163.com>
2021-01-06 19:23:12 +0800

    Add parameter host to startServer().

--- listener/appbase/listener.d
+++ listener/appbase/listener.d
@@ -30,6 +30,12 @@ void startServer(const ushort port, const int businessThreads, const ushort prot
 void startServer(const ushort port, const ushort protocolMagic,
     RequestCallback onRequest, OnSendCompleted onSendCompleted, const int businessThreads = 0, const int workerThreads = 0)
 {
+    startServer("0.0.0.0", port, protocolMagic, onRequest, onSendCompleted, businessThreads, workerThreads);
+}
+
+void startServer(const string host, const ushort port, const ushort protocolMagic,
+    RequestCallback onRequest, OnSendCompleted onSendCompleted, const int businessThreads = 0, const int workerThreads = 0)
+{
     _businessPool    = new ThreadPool((businessThreads < 1) ? (totalCPUs * 2 + 2) : businessThreads);
 
     _protocolMagic   = protocolMagic;
@@ -37,7 +43,7 @@ void startServer(const ushort port, const ushort protocolMagic,
     _onSendCompleted = onSendCompleted;
 
     TcpListener listener = new TcpListener();
-    listener.bind(new InternetAddress("0.0.0.0", port));
+    listener.bind(new InternetAddress(host, port));
     listener.listen(1024);
 
     Codec codec = new Codec(CodecType.SizeGuide, protocolMagic);
